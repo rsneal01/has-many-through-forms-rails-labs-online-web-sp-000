2204cb8904f2abcafe1e76794acd14f6774f9c73
rsneal01 <rsneal01@gmail.com>
2021-06-21 10:55:24 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,17 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :categories
+  # accepts_nested_attributes_for :users
 
   def categories_attributes=(category_attributes)
+    # binding.pry
     category_attributes.values.each do |category_attribute|
-      category = Category.find_or_create_by(category_attribute)
-      self.categories << category
+      if category_attribute[:name].present?
+        category = Category.find_or_create_by(category_attribute)
+        self.categories << category
+      else
+      end
+      
     end
   end
 end
